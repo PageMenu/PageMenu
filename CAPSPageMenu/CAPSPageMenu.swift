d2ec39f046d2186152dee23164b9cbae80682c7f
Niklas Fahl <nfahl@cs.ua.edu>
2014-12-19 10:16:27 -0600

    - added license

--- CAPSPageMenu/CAPSPageMenu.swift
+++ CAPSPageMenu/CAPSPageMenu.swift
@@ -1,10 +1,21 @@
+//  CAPSPageMenu.swift
 //
-//  NFScrollMenuViewController.swift
-//  NFTopMenuController
+//  Niklas Fahl
 //
-//  Created by Niklas Fahl on 12/16/14.
-//  Copyright (c) 2014 Niklas Fahl. All rights reserved.
+//  Copyright (c) 2014 The Board of Trustees of The University of Alabama All rights reserved.
+//    
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 //
+//  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//
+//  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+//  Neither the name of the University nor the names of the contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+//  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import UIKit
 
@@ -26,7 +37,7 @@ class MenuItemView: UIView {
 }
 
 class CAPSPageMenu: UIViewController, UIScrollViewDelegate, UIGestureRecognizerDelegate {
-
+    
     // MARK: - Properties
     
     let menuScrollView = UIScrollView()
@@ -59,7 +70,7 @@ class CAPSPageMenu: UIViewController, UIScrollViewDelegate, UIGestureRecognizerD
     // MARK: - View life cycle
     
     init(viewControllers: [AnyObject]) {
-        super.init()
+        super.init(nibName: nil, bundle: nil)
         
         controllerArray = viewControllers
     }
@@ -233,7 +244,8 @@ class CAPSPageMenu: UIViewController, UIScrollViewDelegate, UIGestureRecognizerD
         if tappedPoint.y < menuScrollView.frame.height {
             
             // Calculate tapped page
-            var itemIndex : Int = Int(tappedPoint.x / 141.0)
+            var itemIndex : Int = Int((tappedPoint.x - menuMargin / 2) / (menuMargin + menuItemWidth))
+            println(itemIndex)
             
             // Update page if changed
             if itemIndex != currentPageIndex {
@@ -249,3 +261,4 @@ class CAPSPageMenu: UIViewController, UIScrollViewDelegate, UIGestureRecognizerD
         }
     }
 }
+
