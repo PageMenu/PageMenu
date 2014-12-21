22b2a341e5d1ad53ed71f50c5999a868e58e8bbd
Niklas Fahl <niklas.fahl@me.com>
2014-12-20 23:24:25 -0600

    - new demo!

---
+++ Demos/Demo Menu Item With Based On Title Text/PageMenuDemoMenuItemWidthBasedOnTitleText/PageMenuDemoMenuItemWidthBasedOnTitleText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageMenuDemoMenuItemWidthBasedOnTitleText
+//
+//  Created by Niklas Fahl on 12/20/14.
+//  Copyright (c) 2014 CAPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
