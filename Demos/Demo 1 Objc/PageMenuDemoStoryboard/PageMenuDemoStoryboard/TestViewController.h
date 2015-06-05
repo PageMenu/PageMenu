1f3713ba4794355bb45114b2f6893364cd2768ad
jinSasaki <sasaki_jin@cyberagent.co.jp>
2015-06-05 17:48:02 +0900

    Fixed Comments

--- Demos/Demo 1 Objc/PageMenuDemoStoryboard/PageMenuDemoStoryboard/TestViewController.h
+++ Demos/Demo 1 Objc/PageMenuDemoStoryboard/PageMenuDemoStoryboard/TestViewController.h
@@ -2,7 +2,7 @@
 //  TestViewController.h
 //  PageMenuDemoStoryboard
 //
-//  Created by 佐々木 尽 on 2015/06/05.
+//  Created by Jin Sasaki on 2015/06/05.
 //  Copyright (c) 2015年 Jin Sasaki. All rights reserved.
 //
 
