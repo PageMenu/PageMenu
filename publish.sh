9ca8b244d498986036efc02a5b11cb53a913ff1e
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2018-02-28 17:02:09 -0300

    Publishing 2.1.4

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@ PUBLISHING_VERSION=`podspec-bump --dump-version`
 git commit -am "Publishing ${PUBLISHING_VERSION}";
 git tag "${PUBLISHING_VERSION}";
 git push --tags;
-pod repo push mv-swift-package-repository PageMenu.podspec --verbose;
-git push origin master;
+pod repo push mv-swift-package-repository project.podspec --verbose --allow-warnings
+git push origin;
