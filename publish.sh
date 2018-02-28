8728444383c26b46d3d73e0661b879bd08ad2df3
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2018-02-28 17:05:44 -0300

    Publishing 2.1.6

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@ PUBLISHING_VERSION=`podspec-bump --dump-version`
 git commit -am "Publishing ${PUBLISHING_VERSION}";
 git tag "${PUBLISHING_VERSION}";
 git push --tags;
-pod repo push mv-swift-package-repository project.podspec --verbose --allow-warnings
+pod repo push mv-swift-package-repository --verbose --allow-warnings
 git push origin;
