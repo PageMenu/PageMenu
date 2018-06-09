6a2d7ff6f5e2d92c03f8cfa369e24ec5606d7b31
加賀谷諒 <ryokagaya0624@gmail.com>
2018-06-09 09:09:27 +0900

    adapt swift4.1

--- Classes/CAPSPageMenu+UIConfiguration.swift
+++ Classes/CAPSPageMenu+UIConfiguration.swift
@@ -186,9 +186,9 @@ extension CAPSPageMenu {
                 let controllerTitle : String? = controller.title
                 
                 let titleText : String = controllerTitle != nil ? controllerTitle! : "Menu \(Int(index) + 1)"
-                
+
                 let itemWidthRect : CGRect = (titleText as NSString).boundingRect(with: CGSize(width: 1000, height: 1000), options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font:configuration.menuItemFont], context: nil)
-                
+
                 configuration.menuItemWidth = itemWidthRect.width
                 
                 menuItemFrame = CGRect(x: totalMenuItemWidthIfDifferentWidths + configuration.menuMargin + (configuration.menuMargin * index), y: 0.0, width: configuration.menuItemWidth, height: configuration.menuHeight)
